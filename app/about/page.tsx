dc5838f98acf5c17015dd9d15a7faf73ffc20e3f
Gabriel Oka <gabukabr@gmail.com>
2025-04-13 20:06:49 -0300

    Sobre mim

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+export default function About() {
+    return (
+      <section>
+        <h2 className="h3 mb-3">Sobre mim</h2>
+        <p>
+        👋 Olá! Eu sou Gabriel Oka, Web Designer com experiência em desenvolvimento de sites e lojas virtuais, sempre focado em unir design e estratégia para criar experiências digitais eficientes.</p>
+
+        <p>💻 Possuo habilidades em WordPress (Elementor), Nuvemshop, Figma, Photoshop e desenvolvimento front-end (HTML, CSS, JS), além de conhecimento em hospedagem e manutenção de sites. Meu diferencial está na visão estratégica dos projetos, buscando sempre melhorar a experiência do usuário e otimizar conversões.</p>
+
+        <p>🎓 Atualmente, estou no último semestre da graduação em Tecnologia em Sistemas para Internet na universidade Senac SP, onde venho aprimorando minhas habilidades técnicas e criativas para oferecer soluções digitais mais completas.</p>
+
+        <p>🚀 Busco uma oportunidade em uma agência onde possa contribuir com minhas habilidades e crescer profissionalmente, agregando valor aos projetos e à equipe.
+        </p>
+      </section>
+    )
+  }
+  
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,8 @@ export default function Navbar() {
         <Link className="navbar-brand" href="/">Gabriel Oka</Link>
         <div className="collapse navbar-collapse d-flex">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-2">
-            <li className="nav-item"><Link className="nav-link" href="/projects">Projetos</Link></li>
+          <li className="nav-item"><Link className="nav-link" href="/about">Sobre</Link></li>
+          <li className="nav-item"><Link className="nav-link" href="/projects">Projetos</Link></li>
             <li className="nav-item"><Link className="nav-link" href="/skills">Skills</Link></li>
             <li className="nav-item"><Link className="nav-link" href="/contact">Contato</Link></li>
           </ul>
